4849c6023796c5a3e4359618ecd507f55d580fc0
jonny <dekan@gaenseklein.net>
2021-03-10 22:45:24 +0100

    activate canvas only when needed

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-function init(){
-
-}
 var inputstream;
+var outputstream;
 function captureVideo(){
   let video = document.getElementById('inputvideo');
     video.addEventListener('canplay',function(){
-      mirror();
+      //mirror(); //as we dont want to mirror alway, only if needed
       document.body.classList.add('video-active');
     })
 
@@ -16,42 +14,42 @@ function captureVideo(){
 }
 
  async function pipClick() {
-  // pipButtonElement.disabled = true;
   console.log('requesting picture in picture')
     resp = await document.getElementById('outputvideo').requestPictureInPicture();
    console.log(resp);
-  // pipButtonElement.disabled = false;
  };
  var ongoingloop=false;
  var loopTimeout;
- async function mirror(){
-   let video = document.getElementById('inputvideo');
-   let canvas = document.getElementById('canvas');
-   let output = document.getElementById('outputvideo');
-   let ctx = canvas.getContext('2d');
-   ctx.translate(video.videoWidth,0);
-   ctx.scale(-1,1);
-   ongoingloop=true;
-   loop = function(){
-     ctx.drawImage(video, 0, 0);
-     if(ongoingloop)loopTimeout = setTimeout(loop,1000/300);
-   }
-   loop();
-   canvasToVideo(false);
+ async function mirror(activateMirror){
+       let video = document.getElementById('inputvideo');
+       let canvas = document.getElementById('canvas');
+       let output = document.getElementById('outputvideo');
+       let ctx = canvas.getContext('2d');
+       ctx.translate(video.videoWidth,0);
+       ctx.scale(-1,1);
+       ongoingloop=true;
+       loop = function(){
+         ctx.drawImage(video, 0, 0);
+         if(ongoingloop)loopTimeout = setTimeout(loop,1000/300);
+       }
+       loop();
+       canvasToVideo(activateMirror);
  }
- var outputstream;
+
  function canvasToVideo(mirroractive){
    let canvas = document.getElementById('canvas');
    outputstream = canvas.captureStream(25);
    changeOutput(mirroractive);
  }
  var isMirror=false;
- function changeOutput(mirror){
-   isMirror=mirror;
-   document.body.classList.toggle('mirror-active',isMirror);
-   let output = document.getElementById('outputvideo');
-   if(mirror)output.srcObject=outputstream;
-   else output.srcObject=inputstream;
+ function changeOutput(toMirror){
+ if(toMirror && !ongoingloop){
+   mirror(true);
+   return;
  }
-
- init();
+ isMirror=toMirror;
+ document.body.classList.toggle('mirror-active',isMirror);
+ let output = document.getElementById('outputvideo');
+ if(isMirror)output.srcObject=outputstream;
+ else output.srcObject=inputstream;
+}
